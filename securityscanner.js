1fe33083197edccc3589fd8ec81284f81586958e
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-10 22:56:39 -0400

    Added two methods inside of object

--- securityscanner.js
+++ securityscanner.js
@@ -6,4 +6,19 @@ let securityScan = {
     employeeNameInBuilding: [],
     badgeIdOutBuilding: [],
     employeeNameOutBuilding: [],
+    inScan: function () {
+
+        this.badgeIdInBuilding.push(this.currentBadgeIsScanned);
+        this.employeeNameInBuilding.push(this.currentEmployeeNameScanned);
+        console.log(`:::::In The Building::::`);
+        console.log(`ID's: ${this.badgeIdInBuilding}`);
+        console.log(`Names: ${this.employeeNameInBuilding}`);
+    },
+    outScan: function () {
+        this.badgeIdOutBuilding.push(this.currentBadgeIsScanned);
+        this.employeeNameOutBuilding.push(this.currentEmployeeNameScanned);
+        console.log(`::::Out The Building::::`);
+        console.log(`ID's: ${this.badgeIdOutBuilding}`);
+        console.log(`Names: ${this.employeeNameOutBuilding}`);
+    }
 }
