810821a8af6ba3d6f638502ad52ef937781b2bde
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-13 20:21:08 -0400

    Fixed console.log and added more arguments

--- securityscanner.js
+++ securityscanner.js
@@ -17,7 +17,7 @@ let securityScan = {
     },
     outScan: function () {
         if (this.badgeIdOutBuilding.includes (this.currentBadgeIdScanned)) {
-            console.log(`:::WARNING ${this.currentEmployeeNameScanned} DID NOT ENTER THE BUILDING GARAGE!:::`);
+            console.log(`:::WARNING ${this.currentEmployeeNameScanned} DID NOT ENTER THE BUILDING!:::`);
         } else
         this.badgeIdOutBuilding.push(   this.badgeIdInBuilding.splice(  this.badgeIdInBuilding.indexOf(this.currentBadgeIdScanned), 1    ).pop()  ); 
         this.employeeNameOutBuilding.push(    this.employeeNameInBuilding.splice(    this.employeeNameInBuilding.indexOf(this.currentEmployeeNameScanned), 1   ).pop()   );
@@ -38,3 +38,10 @@ function scanId (id, name, inout) {
 scanId("1234", "chelsea", "in");
 scanId("1234", "chelsea", "out");
 scanId("2345", "patrick", "in");
+scanId("2345", "patrick", "out");
+scanId("4567", "Margie", "in");
+scanId("4567", "Margie", "in");
+
+
+
+// .push() is printing the findings of the array, while splice is removing what the next method(indexOf) is finding, which is the currentBadgeIdScanned. 1 is telling the array how many elements it wants to remove from array and pop is taking out the last element it finds then making it a string. 
